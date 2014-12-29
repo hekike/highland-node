f5258f060ae4a7535c51a53b74980d46a3bb1813
Peter Marton <peter@risingstack.com>
2014-12-29 09:13:29 +0100

    Server should be more frp

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var fs = require('fs');
+
+var _ = require('highland');
 var WebSocketServer = require('ws').Server;
 var websocket = require('websocket-stream');
 var wss = new WebSocketServer({ port: 3000 });
+var readFile = _.wrapCallback(fs.readFile);
 
 wss.on('connection', function(ws) {
-  var stream = websocket(ws);
-  fs.createReadStream('cats.json').pipe(stream);
+  var wsStream = websocket(ws);
+
+  _(['cats1.json', 'cats2.json'])
+    .map(readFile)
+    .sequence()    
+    .pipe(wsStream);
 });
